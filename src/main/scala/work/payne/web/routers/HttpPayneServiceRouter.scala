2a109643aec5cc7b0bc4f1c3cfd4c042dc10644d
David Payne <david.payne@bigcommerce.com>
2018-05-05 17:18:03 -0700

    playing around with simple routes

--- src/main/scala/work/payne/web/routers/HttpPayneServiceRouter.scala
+++ src/main/scala/work/payne/web/routers/HttpPayneServiceRouter.scala
@@ -1,14 +1,32 @@
 package work.payne.web.routers
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.ContentNegotiator.Alternative.ContentType
+import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 
-import akka.http.scaladsl.server.Directives._
+import scala.concurrent.ExecutionContext
 
-class HttpPayneServiceRouter extends HttpLog{
+class HttpPayneServiceRouter(implicit sys: ActorSystem, ec: ExecutionContext) {
 
   def route(): Route = {
 
+    get { ctx =>
+      val res = getSimpleResponse
+      ctx.complete(res)
+    }
 
-    getFromResourceDirectory("www")
   }
+
+  def getSimpleResponse: HttpResponse = {
+    HttpResponse(
+      status = StatusCodes.OK,
+      entity = HttpEntity(
+        ContentTypes.`text/html(UTF-8)`,
+        "<html><head></head><body><p>I respond for all gets</p></body></html>"
+      )
+    )
+  }
+
 }
